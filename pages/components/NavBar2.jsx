c9e18a175042686beda2e50c6a3eed1c879cbf47
Ivixx09 <estudio.xx23@gmail.com>
2023-05-27 10:24:48 -0300

    design for mobile beta

--- pages/components/NavBar2.jsx
+++ pages/components/NavBar2.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavBar2 = ({nav}) => {
     return (
-        <div className={nav?"mt-8 fixed left-0  w-[100%] sm:w-[50%] bg-black border-gray-700 p-10 z-10 ease-in duration-500":"z-10 fixed top-[-200%] left-0 p-10 ease-in duration-500"}>
+        <div className={nav?"mt-12 fixed left-0  w-[100%] sm:w-[50%] bg-black border-gray-700 p-10 z-10 ease-in duration-500":"z-10 fixed top-[-200%] left-0 p-10 ease-in-out duration-500"}>
             <div className="grid  grid-cols-1 justify-items-center space-y-4">
                 <div className="cursor-pointer w-full grid grid-cols-1 justify-items-center hover:text-gray-400">
                     <span className="text-white">A cerca de mí</span>
