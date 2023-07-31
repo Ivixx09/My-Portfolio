0cc28ff813b71faa7cb6d56f874aef23614ef937
Ivixx09 <estudio.xx23@gmail.com>
2023-07-31 08:44:09 -0300

    Animations added for emails

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -3,20 +3,20 @@ import { MdEmail } from "react-icons/md";
 const Contact = () => {
   return (
     <div className='py-16 bg-[#dee6e9]' id='Contacto'>
-        <div className='flex justify-center'>
+        <div className='flex justify-center animate-bounce'>
             <MdEmail size="7em"/>
         </div>
         <div className='flex flex-col justify-center my-5'>
-            <div className='text-center my-5 font-2ond'>
+            <div className='text-2xl text-center my-5 font-2ond'>
                 Estemos en contacto!
             </div>
-            <div className='text-center my-5 font-2ond'>
+            <div className='text-xl text-center my-5 font-2ond'>
                 Si estás interesado en hablar conmigo por un proyecto o algo similar escríbeme!
             </div>
         </div>
         <div className='flex justify-center cursor-pointer'>
-            <div className='transition hover:bg-blue-600 hover:border-blue-600 hover:text-white border-4 w-[50%] text-center border-black'>
-                <span className='text-3xl font-wde'>
+            <div className='transition duration-300 hover:bg-blue-600 hover:border-blue-600 hover:text-white border-4 w-[50%] text-center border-black'>
+                <span className='text-3xl font-wde animate-pulse'>
                     Enviar un mensaje
                 </span>
             </div>
