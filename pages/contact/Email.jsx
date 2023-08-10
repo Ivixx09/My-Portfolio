420037f13e3938e38a2b61760104e0cc5037b1b3
Ivixx09 <estudio.xx23@gmail.com>
2023-08-10 10:24:02 -0300

    Bug for importation fixed

--- pages/contact/Email.jsx
+++ pages/contact/Email.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Formik, Form, Field, useFormik } from 'formik'
-import { mailSchema } from '../Validations/Validations'
+import mailSchema from '../Validations/Validations'
 
 export default function Page() {
   return (
